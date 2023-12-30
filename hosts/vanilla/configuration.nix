d36d1df2121ba2bc73fc77febbac772e509d1097
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-12-30 20:28:07 +1000

    vanilla: changed to fhs vscodium

--- hosts/vanilla/configuration.nix
+++ hosts/vanilla/configuration.nix
@@ -131,13 +131,13 @@ in
       ];
     };
 
-    vscodium = pkgs.vscode-with-extensions.override {
-        vscode = pkgs.vscodium;
-	vscodeExtensions = with pkgs.vscode-extensions; [
-	  enkia.tokyo-night
-	  redhat.java
-	];
-      };
+    vscodium-fhs = pkgs.vscodium-fhs.override {
+      vscode = pkgs.vscodium-fhs;
+	    vscodeExtensions = with pkgs.vscode-extensions; [
+	      enkia.tokyo-night
+	      redhat.java
+	    ];
+    };
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
