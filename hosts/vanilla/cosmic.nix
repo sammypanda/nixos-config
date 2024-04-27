5736d1a8692ed4049bac1107aba02012838e7a0f
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-04-27 10:35:49 +1000

    added additional basic Cosmic Desktop apps

--- hosts/vanilla/cosmic.nix
+++ hosts/vanilla/cosmic.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
     services = {
         desktopManager.cosmic.enable = true;
         displayManager.cosmic-greeter.enable = true;
     };
+
+    # Available software that isn't enabled by default
+    environment.systemPackages = with pkgs; [
+        cosmic-design-demo
+        cosmic-emoji-picker
+        cosmic-protocols
+        cosmic-tasks
+    ];
 }
